36fb68dcf9961cf1c0c7154ef02b55246691ba14
Franciszek Czajka <franekczajkaa@gmail.com>
2024-01-03 19:10:53 +0100

    Add ability to sign out

--- src/routes/sign-out/index.tsx
+++ src/routes/sign-out/index.tsx
@@ -1,5 +1,29 @@
 import { component$ } from "@builder.io/qwik";
+import { server$, useNavigate } from "@builder.io/qwik-city";
+
+const signOut = server$(function (cookieName: string) {
+  this.cookie.delete(cookieName, {
+    path: "/",
+  });
+  if (this.cookie.get(cookieName)) return true;
+
+  return false;
+});
 
 export default component$(() => {
-  return <div>Sign out</div>;
+  const nav = useNavigate();
+  return (
+    <div class="flex">
+      <button
+        class="btn"
+        type="submit"
+        onClick$={async () => {
+          const isSignedOut = await signOut("jwt");
+          if (!isSignedOut) nav("/");
+        }}
+      >
+        Sign out
+      </button>
+    </div>
+  );
 });
