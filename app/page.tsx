b74fa81225f1ada33b945812df2a2634ce2e2f73
Franciszek Czajka <franekczajkaa@gmail.com>
2024-12-26 15:24:53 +0100

    Add pages

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,17 @@ export default function Home() {
   return (
     <div>
       <h1 className='font-default'>Home Page</h1>
-      <Link className='font-headline' href='/profile'>
-        Profile page
+      <Link className='font-headline' href='/transactions'>
+        Transactions page
+      </Link>
+      <Link className='font-headline' href='/dashboard'>
+        Dashboard page
+      </Link>
+      <Link className='font-headline' href='/about'>
+        About page
+      </Link>
+      <Link className='font-headline' href='/categories'>
+        Categories page
       </Link>
     </div>
   );
--- lib/db.ts
+++ lib/db.ts
@@ -8,6 +8,7 @@ declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
